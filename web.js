c2ff5072fbf6cff9f94967dcbc956535071cf663
Kyemenu-Sarsah <papasarsah@gmail.com>
2013-07-15 22:16:11 +0000

    Made changes to web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var thestring = fs.readFileSync('index.html',encoding);
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(thestring);
 });
 
 var port = process.env.PORT || 5000;
